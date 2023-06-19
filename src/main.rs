048c55a791cb79a05f8561e38de38066bae935a3
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-06-19 17:20:38 +0100

    lifetimes with structs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-fn main() {
-    let result;
-    let string1 = String::from("long string is long");
-    let string2 = String::from("xyz");
-    {
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
-}
+use std::fmt::Display;
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
